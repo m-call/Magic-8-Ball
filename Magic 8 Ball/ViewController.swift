6cf67ef049a6448fe2cbb4b844ee9c4de5099549
Michael Callahan <mcallahan0@yahoo.com>
2022-01-13 02:52:33 -0500

    Added randomization of the 8 ball image with the randomElement() method

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     let ballArr = [ #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball5") ]
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        
+        eightBallImageView.image = ballArr.randomElement()
     }
     
 }
